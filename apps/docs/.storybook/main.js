5b4c6bb0a8aade34efabdc6af3df4a9dea0ea3b4
Sunset-kim <wood2002@naver.com>
2023-07-13 00:24:18 +0900

    chore: change package name

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@sunset-kim/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/sunset-kim-core/"
             ),
           },
         ],
